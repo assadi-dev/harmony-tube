789f4d8d859180065977ebc1ad20a28955ce66d4
assadi-dev <assadi.halifa@gmail.com>
2025-10-26 12:55:15 +0100

    documentation du model track

---
+++ lib/screens/home/more_track_actions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoreTrackActions extends StatelessWidget {
+  const MoreTrackActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
