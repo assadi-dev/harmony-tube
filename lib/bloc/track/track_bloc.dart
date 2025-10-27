f71be88723d454f44633215cb0cc05840623af24
assadi-dev <assadi.halifa@gmail.com>
2025-10-27 17:32:28 +0100

    mise en place crud des track avec bloc

--- lib/bloc/track/track_bloc.dart
+++ lib/bloc/track/track_bloc.dart
@@ -1,7 +1,70 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:harmony_tube/bloc/track/track_event.dart';
 import 'package:harmony_tube/bloc/track/track_state.dart';
+import 'package:harmony_tube/core/models/local_track.dart';
+import 'package:harmony_tube/mocks/songs.dart';
+
 
 class TrackBloc extends Bloc<TrackEvent, TrackState> {
-  TrackBloc() : super( TrackState());
+  TrackBloc() : super(const TrackState()) {
+    on<GetAllTrackCollections>(retrieveAllTrackCollections);
+    on<AddTrack>(addTrack);
+    on<DeleteTrack>(removeTrack);
+  }
+
+
+  Future<void> retrieveAllTrackCollections(TrackEvent event,
+      Emitter<TrackState> emit) async {
+     List<TrackItemModel> collections = state.collections ?? [];
+    Exception? error;
+    try {
+      emit(state.copyWith(error: null, isLoading: true));
+      //TODO Call API to retrieve all track collections
+      collections = youtubeTrackMock;
+
+    }
+    catch (e) {
+      error = Exception(e);
+    } finally {
+      emit(state.copyWith(
+          isLoading: false, collections: collections, error: error));
+    }
+  }
+
+
+  void addTrack(AddTrack event, Emitter<TrackState> emit) {
+    final List<TrackItemModel> updatedCollections = [
+      ...state.collections ?? []
+    ];
+    Exception? error;
+    try {
+      final newTrack = event.track;
+      updatedCollections.add(newTrack);
+    } catch (e) {
+      error = Exception(e);
+    } finally {
+      emit(state.copyWith(
+          collections: updatedCollections, error: error, isLoading: false));
+    }
+  }
+
+  void removeTrack(DeleteTrack event, Emitter<TrackState> emit) {
+
+    final List<TrackItemModel> updatedCollections = [
+      ...state.collections ?? []
+    ];
+    Exception? error;
+    try {
+     final String trackId = event.trackId;
+      updatedCollections.removeWhere((track) => track.id == trackId);
+    } catch (e) {
+      error = Exception(e);
+    } finally {
+      emit(state.copyWith(
+          collections: updatedCollections, error: error, isLoading: false));
+    }
+
+  }
 }
--- lib/bloc/track/track_event.dart
+++ lib/bloc/track/track_event.dart
@@ -1,1 +1,40 @@
-class TrackEvent {}
+import 'package:equatable/equatable.dart';
+import 'package:harmony_tube/core/models/local_track.dart';
+
+class TrackEvent extends Equatable {
+
+  final TrackItemModel? track;
+
+  const TrackEvent({ this.track});
+
+  @override
+  List<Object?> get props => [track!];
+}
+
+
+class GetAllTrackCollections extends TrackEvent {
+  const GetAllTrackCollections();
+}
+
+class FindTrack extends TrackEvent {
+  const FindTrack(TrackItemModel track);
+}
+
+class DeleteTrack extends TrackEvent {
+  final String trackId;
+  const DeleteTrack({required this.trackId});
+}
+
+class AddTrack extends TrackEvent {
+  final TrackItemModel track;
+  const AddTrack({required this.track});
+}
+
+class UpdateTrack extends TrackEvent {
+  final TrackItemModel track;
+  const UpdateTrack({required this.track});
+}
+
+class ClearTracksCollections extends TrackEvent {
+  const ClearTracksCollections();
+}
